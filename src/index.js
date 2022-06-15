fc3d79ae009b82104c11e521a45bcc3b3a2d0418
Jared <jared.wilson4332@gmail.com>
2022-06-14 20:42:47 -0600

    Flesh out adding tasks and get started on creating projects

--- src/index.js
+++ src/index.js
@@ -1,37 +1,23 @@
 import './style.css'
 
-
-// build task
-// name, description, date
-//check mark moves task to bottom, X removes it entirely
-// sort by date if possible
-
 // sort script by task stuff, project stuff, page appearances,
-//tying stuff together, like with event listeners
+// tying stuff together, like with event listeners
 // figure out what makes the most sense as you go along
 
-// get form values
-//turn values into "task"
-// turn task into display
 
 const getTaskValues = () => {
-    let taskName = document.getElementById('task')
-    let taskDesc = document.getElementById('desc')
-    let taskDate = document.getElementById('date')
-    return taskName, taskDesc, taskDate
+    let taskName = document.getElementById('task').value
+    let taskDesc = document.getElementById('desc').value
+    let taskDate = document.getElementById('date').value
+    return [taskName, taskDesc, taskDate]
 }
-// follow by calling task maker
-
 
 const taskMaker = (name, desc, date) => {
     return { name, desc, date }
 }
 
-let newTask = taskMaker("laundry", "socks", "2023-01-01")
-
 
 const taskContainer = document.getElementById('taskContainer')
-
 
 const displayTask = (task) => {
     let newTask = document.createElement('div')
@@ -41,23 +27,16 @@ const displayTask = (task) => {
     let newTaskName = createThing(task.name, 'p', "taskName", newTask)
     let newTaskDesc = createThing(task.desc, 'p', "taskDesc", newTask)
     let newTaskDate = createThing(task.date, 'p', "taskDate", newTask)
+    let checkmark = createThing('', 'button', "taskCheck", newTask)
+    let deleteTask = createThing('', 'button', "taskDelete", newTask)
+    let starTask = createThing('', 'button', "taskPriority", newTask)
 }
-
 
 const createThing = (text, type, group, parent) => {
     let newThing = document.createElement(type)
     newThing.textContent = text
     newThing.classList.add(group)
-    newThing.appendChild(parent)
-}
-
-displayTask(newTask)
-
-
-
-const projectMaker = (name) => {
-//    let name = []
-//    return { name }
+    parent.appendChild(newThing)
 }
 
 
@@ -79,8 +58,8 @@ const taskPromptHide = () => {
 
 //temp
 const funnyFunction = () => {
-    getTaskValues()
-    let funnyTask = taskMaker(taskName, taskDesc, taskDate)
+    let funnyArray = getTaskValues()
+    let funnyTask = taskMaker(funnyArray[0], funnyArray[1], funnyArray[2])
     displayTask(funnyTask)
     return false
 }
@@ -89,3 +68,66 @@ const funnyFunction = () => {
 newTaskButton.addEventListener("click", taskPromptReveal)
 addTaskCancel.addEventListener("click", taskPromptHide)
 addTaskAdd.addEventListener("click", funnyFunction)
+
+
+let newTask = taskMaker("Do laundry", "I need to wash all my socks", "2023-01-01")
+displayTask(newTask)
+
+const sidebar = document.getElementById("sidebar")
+const sidebarToggleButton = document.getElementById("sidebarToggle")
+
+const toggleThing = (element) => {
+    if (element.style.display != 'none') {
+        element.style.display = 'none'
+    } else {
+        element.style.display = block
+    }
+}
+
+// sidebarToggleButton.addEventListener("click", toggleThing(sidebar))
+// figure out applicable grid layout
+
+// project maker logic
+// just assign things certain classes and make them appear via selection of those
+//projects on the side
+// projects can simply be buttons
+// depending which project is "active" is what will be shown
+// or just uhh use arrays
+
+const getProjectValues = () => {
+    return document.getElementById('title').value
+}
+
+//const projectMaker = (title) => {
+//    return { title }
+//}
+
+const projectArray = (title) => {
+    title = []
+    return { title }
+}
+
+
+
+const projectContainer = document.getElementById('projectContainer')
+
+const projectDisplay = (project) => {
+    createThing(project, 'button', "project", projectContainer)
+}
+
+const projectDisplayTasks = (array) => {
+    for (let i = 0; i < array.length; i++) {
+        displayTask(array[i])
+    }
+}
+
+
+
+const projectAdd = () => {
+    let newProject = getProjectValues()
+    console.log(newProject)
+    projectDisplay(newProject)
+}
+
+const addProjectButton = document.getElementById('addProjectAdd')
+addProjectButton.addEventListener("click", projectAdd)
