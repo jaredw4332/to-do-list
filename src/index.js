8fd8ab4b0011848fabf6c018696e4412b34ff45f
Jared <jared.wilson4332@gmail.com>
2022-06-18 20:35:09 -0600

    Add priority functionality, expand on project capabilities

--- src/index.js
+++ src/index.js
@@ -11,7 +11,9 @@ import './style.css'
 // similar to tic tac toe winning logic
 
 
-let allTaskArray = []
+const allTaskArray = []
+const priorityArray = []
+const allProjectArray = []
 let mainArray = undefined
 
 //task
@@ -40,8 +42,42 @@ const displayTask = (task) => {
     let newTaskDesc = createThing(task.desc, 'p', "taskDesc", newTask)
     let newTaskDate = createThing(task.date, 'p', "taskDate", newTask)
     let checkmark = createThing('', 'button', "taskCheck", newTask)
-    let deleteTask = createThing('', 'button', "taskDelete", newTask)
-    let starTask = createThing('', 'button', "taskPriority", newTask)
+    priorityTaskButton(newTask, task)
+    deleteTaskButton(newTask, task)
+}
+
+const priorityTaskButton = (parent, task) => {    
+    let priorityTask = document.createElement('button')
+    priorityTask.classList.add("taskPriority")
+    parent.appendChild(priorityTask)
+    priorityTask.addEventListener("click", function() {
+        if (priorityArray.indexOf(task) == -1) {
+            priorityArray.push(task)
+            priorityTask.classList.add("taskPriorityActive")
+        } else {
+            let index = priorityArray.indexOf(task)
+            priorityArray.splice(index, 1)
+            priorityTask.classList.remove("taskPriorityActive")
+        }
+    })
+    checkPriority(priorityTask, task)
+}
+
+const deleteTaskButton = (parent, task) => {
+    let deleteTask = document.createElement('button')
+    deleteTask.classList.add("taskDelete")
+    parent.appendChild(deleteTask)
+    deleteTask.addEventListener("click", function() {
+        let index = allTaskArray.indexOf(task)
+        allTaskArray.splice(index, 1)
+        taskContainer.removeChild(parent)
+    })
+}
+
+const checkPriority = (button, task) => {
+    if (priorityArray.indexOf(task) > -1){
+        button.classList.add("taskPriorityActive")
+    }
 }
 
 const createThing = (text, type, group, parent) => {
@@ -67,7 +103,7 @@ const toggleTaskPrompt = () => {
 const addTaskToArray = (task) => {
     allTaskArray.push(task)
     let activeButton = document.getElementsByClassName('activeProject')
-        if (activeButton == undefined) {
+        if (mainArray == allTaskArray) {
             return
         } else {
             mainArray.push(task)
@@ -79,7 +115,7 @@ const addTaskToArray = (task) => {
 const taskAdd = () => {
     let taskArray = getTaskValues()
     let task = taskMaker(taskArray[0], taskArray[1], taskArray[2])
-    displayTask(task)
+    let taskDiv = displayTask(task)
     addTaskToArray(task)
     clearInput("taskInput")
     toggleTaskPrompt()
@@ -90,10 +126,6 @@ const taskAdd = () => {
 newTaskButton.addEventListener("click", toggleTaskPrompt)
 addTaskCancel.addEventListener("click", toggleTaskPrompt)
 addTaskAdd.addEventListener("click", taskAdd)
-
-//example
-let newTask = taskMaker("Do laundry", "I need to wash all my socks", "2023-01-01")
-displayTask(newTask)
 
 
 // display
@@ -147,11 +179,11 @@ const projectMaker = (title) => {
     return {title, tasks}
 }
 
-const projectDisplay = (project) => {
-    let newProject = document.createElement('div')
+const projectDisplay = (project, parent) => {
+    let newProject = document.createElement('button')
     newProject.textContent = project
     newProject.classList.add('project')
-    projectContainer.appendChild(newProject)
+    parent.appendChild(newProject)
 
     return newProject
 }
@@ -168,19 +200,24 @@ const projectDeleteButton = (button, array) => {
     })
 }
 
-const projectActiveButton = (project, array) => {
+const projectClickHandler = (project, array, title) => {
     project.addEventListener("click", function() {
+        projectAppendTitle(title)
         mainArray = array
         projectDeactivate()
-        project.classList.add('activeProject')
-        project.disabled = true
+        projectActivate(project)
     })
+}
+
+const projectActivate = (project) => {
+    project.classList.add('activeProject')
+    project.disabled = true
 }
 
 const projectDisplayTasks = (button, array) => {
     button.addEventListener("click", function() {
         projectHideTasks()
-        for (let i = 0; i < array.length; i++) {
+        for (let i = array.length-1; i >= 0; i--) {
             displayTask(array[i])
         }
     })
@@ -188,9 +225,10 @@ const projectDisplayTasks = (button, array) => {
 
 const projectHideTasks = () => {
     let activeTasks = document.getElementsByClassName('newTaskContainer')
-    for (let i=activeTasks.length-1; i>=0; i--) {
+    for (let i = activeTasks.length-1; i >= 0; i--) {
         taskContainer.removeChild(activeTasks[i])
     }
+    return
 }
 
 const projectDeactivate = () => {
@@ -201,21 +239,40 @@ const projectDeactivate = () => {
     }
 }
 
-// task adder searches for activeproject class, and adds it to wherever it finds it
-// if it cant find one it means it's a general task
-
-
 // project logic
 const projectAdd = () => {
     let newProject = getProjectValues()
     newProject = projectMaker(newProject)
-    let newProjectButton = projectDisplay(newProject.title)
-    projectActiveButton(newProjectButton, newProject.tasks)
+    let newProjectButton = projectDisplay(newProject.title, projectContainer)
+    projectClickHandler(newProjectButton, newProject.tasks, newProject.title)
     projectDisplayTasks(newProjectButton, newProject.tasks)
     projectDeleteButton(newProjectButton, newProject.tasks)
     clearInput("projectInput")
     toggleProjectPrompt()
 }
 
+const sidebarMain = document.getElementById("sidebarMain")
+
+// dev use, page functionality
+const customProjectAdd = (title, array) => {
+    let customProjectButton = projectDisplay(title, sidebarMain)
+    projectClickHandler(customProjectButton, array, title)
+    projectDisplayTasks(customProjectButton, array)
+    // remove from final project but keep to style delete button
+    projectDeleteButton(customProjectButton, array)
+    return customProjectButton
+}
+
 const addProjectAdd = document.getElementById('addProjectAdd')
 addProjectAdd.addEventListener("click", projectAdd)
+
+
+const mainTitle = document.getElementById("mainTitle")
+
+const projectAppendTitle = (title) => {
+    mainTitle.textContent = title
+}
+
+customProjectAdd("All", allTaskArray).click()
+customProjectAdd("Priority", priorityArray)
+displayTask("cheese")
